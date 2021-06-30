364c67ab731d8a149e6d7acf5a06aff770c5ef62
Claudi4 <clau.ale.20a@gmail.com>
2021-06-30 11:08:14 -0500

    create peliculas page

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 import { example } from './data.js';
 // import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
+import data from './data/ghibli/ghibli.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, data);
+//linkea titulo de mi html a mi js';
+let titulo = document.getElementById("title");  
+
+//linkea todo el html  y lo trae al Js';
+titulo.innerHTML = data.films[0].title;
+console.log(example, data.films);
+console.log(example, data.films[0].title);
