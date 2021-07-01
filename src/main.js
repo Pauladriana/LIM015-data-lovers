77c24ae2af14c4db80358894dc983e7faf7f5efa
Pauladriana <85115054+Pauladriana@users.noreply.github.com>
2021-07-01 10:20:20 -0500

    Modified main.js to show the movies and characters

--- src/main.js
+++ src/main.js
@@ -1,16 +1,14 @@
 import data from './data/ghibli/ghibli.js';
 
-//linkea titulo de mi html a mi js';
-//let titulo = document.getElementById("title");  
-
-//linkea todo el html  y lo trae al Js';
-/*titulo.innerHTML = data.films[0].title;
-console.log(example, data.films);
-console.log(example, data.films[0].title);*/
-
 //Importamos la data de Ghibli
 const filmTitles = data.films.map((film) => film.title);
 const filmPosters = data.films.map((film) => film.poster);
+const filmDescription = data.films.map((film) => film.description);
+const people = data.films.map((film) => film.people);
+console.log(people)
+
+
+
 
 //BUCLEANDO PARA CREAR ELEMENTOS
 for (let i = 0; i < filmTitles.length; i++) {
@@ -24,6 +22,7 @@ for (let i = 0; i < filmTitles.length; i++) {
     //Creamos el elemento de titulo
     const newMovieTitle = document.createElement("p");
     const titleText = document.createTextNode(filmTitles[i]);
+    newMovieTitle.setAttribute("class", "filmClick");
     //Unimos los elementos al contenedor
     newMovieTitle.appendChild(titleText);
     newMovieCard.appendChild(newMoviePoster);
@@ -31,13 +30,41 @@ for (let i = 0; i < filmTitles.length; i++) {
     //Ubicamos el contenedor en el DOM
     const newMovieContainer = document.getElementsByClassName("cardscontainer")[0];
     newMovieContainer.appendChild(newMovieCard);
+
+    //PAGINA DE FILM
+    document.getElementsByClassName("filmClick")[i].addEventListener("click", function() {
+        document.getElementById("filmdescription").innerHTML = filmDescription[i];
+        document.getElementById("imgFilms").setAttribute("src", filmPosters[i]);
+        document.getElementById("selectedfilm").innerHTML = filmTitles[i];
+
+        //personajes
+        for (let j = 0; j < people.length; j++) {
+          const peopleName = people[i][j].name;
+          console.log(peopleName);
+          const peoplePhoto = people[i][j].img;
+          console.log(peoplePhoto);
+        //Creamos el contenedor
+        const newPeopleCard = document.createElement("div");
+        newPeopleCard.setAttribute("class", "peoplecard");
+        //Creamos el elemento de imagen
+        const newPeoplePhoto = document.createElement("img");
+        newPeoplePhoto.setAttribute("src", peoplePhoto);
+        newPeoplePhoto.setAttribute("class", "people-photo");
+        //Creamos el elemento de titulo
+        const newPeopleName = document.createElement("p");
+        const nameText = document.createTextNode(peopleName);
+        //Unimos los elementos al contenedor
+        newPeopleName.appendChild(nameText);
+        newPeopleCard.appendChild(newPeoplePhoto);
+        newPeopleCard.appendChild(newPeopleName);
+        //Ubicamos el contenedor en el DOM
+        const newPeopleContainer = document.getElementsByClassName("peoplecontainer")[0];
+        newPeopleContainer.appendChild(newPeopleCard);
+        }
+    })
 }
 
 //BOTONES DE FILTRO
-
-/*document.getElementById("directorfilter").addEventListener("click", function() {
-    document.getElementsByClassName("filteritem")[0].style.display = "block";
-});*/
 
 document.getElementById("directorfilter").addEventListener("click", function() {
     if (document.getElementsByClassName("filteritem")[0].style.display === "block") {
@@ -52,22 +79,6 @@ document.getElementById("productorfilter").addEventListener("click", function() 
         document.getElementsByClassName("filteritem")[1].style.display = "none";
     } else {
         document.getElementsByClassName("filteritem")[1].style.display = "block";
-    }
-});
-
-document.getElementById("yearfilter").addEventListener("click", function() {
-    if (document.getElementsByClassName("filteritem")[2].style.display === "block") {
-        document.getElementsByClassName("filteritem")[2].style.display = "none";
-    } else {
-        document.getElementsByClassName("filteritem")[2].style.display = "block";
-    }
-});
-
-document.getElementById("scorefilter").addEventListener("click", function() {
-    if (document.getElementsByClassName("filteritem")[3].style.display === "block") {
-        document.getElementsByClassName("filteritem")[3].style.display = "none";
-    } else {
-        document.getElementsByClassName("filteritem")[3].style.display = "block";
     }
 });
 
