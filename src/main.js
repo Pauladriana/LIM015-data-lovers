eda09ae3f67a86f2b70b825cba353aa1928f6f63
Pauladriana <85115054+Pauladriana@users.noreply.github.com>
2021-07-23 19:06:22 -0500

    Trying to show on ghpages 3

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
-import data from '../data/ghibli/ghibli.js';
-import {filterByDirector, filterByProducer, ordenAlfaAtoZ, ordenAlfaZtoA, yearMovie, scoreMovie} from '../data.js';
+import data from '../src/data/ghibli/ghibli.js';
+import {filterByDirector, filterByProducer, ordenAlfaAtoZ, ordenAlfaZtoA, yearMovie, scoreMovie} from '../src/data.js';
 
 let films = data.films;
 export const allFilms = data.films;
